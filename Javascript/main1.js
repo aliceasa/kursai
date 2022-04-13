a6bd7a72fe0126266d879c85929011bc9a4d0dfd
Alicija <aknolia@rambler.ru>
2022-04-13 14:15:13 +0300

    skaiciuotuvas

--- Javascript/main1.js
+++ Javascript/main1.js
@@ -1,9 +1,24 @@
 const numberArea = document.getElementById(`numberArea`);
+let firstNumber;
+
 function addNumber(number) {
-  if (Number(numberArea.textContent) == 0 ) numberArea.textContent = number;
-} else {
+  if (Number(numberArea.textContent) == 0) {
+    numberArea.textContent = number;
+  } else {
     numberArea.textContent += number;
+  }
 }
+
 function deleteNumber() {
-    numberArea.textContent = numberArea.textContent.slice(0, -1);
+  numberArea.textContent = numberArea.textContent.slice(0, -1);
+}
+
+function add() {
+  numberArea.textContent = Number(numberArea.value);
+  numberArea.textContent = 0;
+}
+
+function equals() {
+  numberArea.textContent = firstNumber + Number(numberArea.textContent);
+  console.log(firstNumber, Number(numberArea.textContent));
 }
